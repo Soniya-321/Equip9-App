11fef41d526466b12b25dd011963ceb6c8632ac2
Soniya <boralakuntasoniya@gmail.com>
2025-01-09 09:59:24 +0530

    full stack

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
+const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
+// Allow requests from your frontend
+const corsOptions = {
+  origin: 'http://localhost:3000', // Replace with your frontend's URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  credentials: true, // Include credentials like cookies, if needed
+};
+
+
 // Middleware
 app.use(bodyParser.json());
+app.use(cors(corsOptions));
 
 // Routes
 app.use('/api', userRoutes);
